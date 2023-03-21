311d282fb44c745caa7347484f72a8ac6fc90e7e
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 06:17:19 +0700

    Built-in Types P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,33 +1,8 @@
 /*
-  Configuring TS Compiler
-  > tsc --init
+  Built-in Types P1
 
-  - safest option for old browser:
-    + "target": "es2016",   
-    + "module": "commonjs",  > learn later
-
-  - some other options: 
-    + "rootDir": "./src"
-    + "outDir": "./dist", 
-    + "removeComments": true 
-    + "noEmitOnError": true > if there is error, then don't generate JS file
-
-  - tsc
-
-  /////////////////////////////
-
-  Debug
-  - tsconfig > sourceMap: true
-    > this is not for us to understand, but for editor to understand
-  - Debug > Create a launch.json file > NodeJS
-    > add 1 more line: "preLaunchTask": "tsc: build - tsconfig.json",
-    > ctrl + shift + B to start the task
-
-  
 */
 
-console.log('Hello World')
-
-let age: number = 20
-
-if (age < 50) age += 10
+let sales: number = 123_456_758
+let course: string = 'Typescript'
+let isPublished: boolean = true
