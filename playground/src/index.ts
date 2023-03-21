d4d37e07116e7e543c51cd3d9440b17f541bac1e
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 06:20:01 +0700

    The any Type P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,13 +1,10 @@
 /*
-  Built-in Types P2
+  The any Type P1
+  - any: we can change the type > it against the idea behind TS
+    > avoid using "any"
 
 */
 
-// if we initialize a variable with value > TS knows the type of that variable > we don't need to annotate > inference > hover on the variable > type = number
-let sales = 123_456_758
-
-let course: string = 'Typescript'
-let isPublished: boolean = true
-
-// type = any > since we did not initialize variable
 let level
+level = 1
+level = 'a'
