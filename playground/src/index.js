f8a267539f0828456e5ef630b7cbe6b2e61fcf5a
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 05:55:03 +0700

    Your First TS Program

---
+++ playground/src/index.js
@@ -0,0 +1,9 @@
+/*
+  Your First TS Program
+  - learn typescript-studies (github) first
+
+    > tsc src/index.ts
+*/
+console.log('Hello World');
+var age = 20;
+age = 'a';
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,11 @@
 /*
+  Your First TS Program
+  - learn typescript-studies (github) first 
 
-
+    > tsc src/index.ts
 */
+
+console.log('Hello World')
+
+let age: number = 20
+age = 'a'
