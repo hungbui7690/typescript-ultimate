beccb8a9239a33023d88fe950fafd568873c7afc
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 06:02:27 +0700

    Configuring TS Compiler

--- playground/src/index.js
+++
@@ -1,9 +0,0 @@
-/*
-  Your First TS Program
-  - learn typescript-studies (github) first
-
-    > tsc src/index.ts
-*/
-console.log('Hello World');
-var age = 20;
-age = 'a';
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,33 @@
 /*
-  Your First TS Program
-  - learn typescript-studies (github) first 
+  Configuring TS Compiler
+  > tsc --init
 
-    > tsc src/index.ts
+  - safest option for old browser:
+    + "target": "es2016",   
+    + "module": "commonjs",  > learn later
+
+  - some other options: 
+    + "rootDir": "./src"
+    + "outDir": "./dist", 
+    + "removeComments": true 
+    + "noEmitOnError": true > if there is error, then don't generate JS file
+
+  - tsc
+
+  /////////////////////////////
+
+  Debug
+  - tsconfig > sourceMap: true
+    > this is not for us to understand, but for editor to understand
+  - Debug > Create a launch.json file > NodeJS
+    > add 1 more line: "preLaunchTask": "tsc: build - tsconfig.json",
+    > ctrl + shift + B to start the task
+
+  
 */
 
 console.log('Hello World')
 
 let age: number = 20
-age = 'a'
+
+if (age < 50) age += 10
