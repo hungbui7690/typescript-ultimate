fe8810906f099705def3dda7e5b8553eb7dd5f7d
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:18:21 +0700

    Generic Interfaces P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,13 @@
 /*
-  Generic Interfaces P1
+  Generic Interfaces P2
   - Assume that we have 2 routes: 
     + https://mywebsite.com/users
     + https://mywebsite.com/products
 
 */
 
+// in case we have error, then data = null > if we have data, then error = null
 interface Result<T> {
-  data: T
+  data: T | null
+  error: string | null
 }
