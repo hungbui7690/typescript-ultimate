f561d69f29dabdc47aea7fc61186bea6d9fc66f0
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 14:50:46 +0700

    Abstract Classes & Methods P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,10 +1,10 @@
 /*
-  Abstract Classes & Methods P1
+  Abstract Classes & Methods P2
 
 */
 
-// at this time, we don't know which shape is this class > we don't know how to write render()
-class Shape {
+// so we use abstract keyword to define abstract class > can't create new instance from abstract class
+abstract class Shape {
   constructor(public color: string) {}
 
   render() {}
@@ -15,12 +15,11 @@ class Circle extends Shape {
     super(color)
   }
 
-  // because this is a circle, we know its shape, so we can render
   override render(): void {
     console.log('Rendering a Circle')
   }
 }
 
-// (***) problem here > shape is not a real thing, unlike circle > we should not allow to create a new shape
+// (***) complain
 let shape = new Shape('red')
 shape.render()
