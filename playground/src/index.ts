27826286464454185dbb2dc3bd96bc7b49323799
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:23:36 +0700

    Generic Constraints P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,12 +1,13 @@
 /*
-  Generic Constraints P1
+  Generic Constraints P2
 
 */
 
-function echo<T>(value: T): T {
+// using extends to limit the types
+function echo<T extends string | number>(value: T): T {
   return value
 }
 
-// what if we want to limit the type that we pass in
 const result1 = echo('1')
 const result2 = echo(1)
+const result3 = echo(true) // only string | number
