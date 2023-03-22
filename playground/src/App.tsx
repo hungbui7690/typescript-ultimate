250a97f8130ab2875ff5b8aa23655ae89475e2ef
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 05:37:27 +0700

    Adding Bootstrap

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,31 +1,18 @@
 /*
-  Create React App with TS
-  > npx create-react-app . --template typescript
+  Adding Bootstrap
+  > npm install bootstrap
 
-  - we have tsx instead of jsx
+  - index.tsx > import
+
 */
 
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className='btn btn-primary'>Click Me</button>
     </div>
   )
 }
