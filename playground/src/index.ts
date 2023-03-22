dfad34ee496a374b06bef8d8d8faa7fe0d674bad
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:12:43 +0700

    Generic Functions P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,16 +1,11 @@
 /*
-  Generic Classes P2
+  Generic Functions P1
 
 */
 
-// <K, V>
-class KeyValuePair<K, V> {
-  constructor(public key: K, public value: V) {}
+function wrapInArray(value: number) {
+  return [value]
 }
 
-// because the type is generic > we can use any type we want
-let pair1 = new KeyValuePair<string, string>('1', 'Apple')
-let pair2 = new KeyValuePair<number, boolean>(1, true)
-
-// (***) if we don't supply the type, TS will infers it if we initialize
-let pairX = new KeyValuePair('1', 'orange')
+// with this implementation, we cannot pass the string > since the declared type is number
+let numbers = wrapInArray(1)
