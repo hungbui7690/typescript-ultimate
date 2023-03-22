de1f0887bdc89e9eb3e1c808fc8a69cd5ebff04f
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:09:21 +0700

    Generic Classes P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,16 +1,20 @@
 /*
-  Understand the Problem P3
+  Generic Classes P1
+  - T: template class > from C++
+    > can use G or T
+
+  > with the implementation below, we don't need to use any type, and don't duplicate the code
 
 */
 
-class KeyValuePair {
-  constructor(public key: number, public value: string) {}
+// we need to add type T in 2 places
+class KeyValuePair<T> {
+  constructor(public key: T, public value: string) {}
 }
 
-// solution 2: create a duplicated class with string as key
-class StringKeyValuePair {
-  constructor(public key: string, public value: string) {}
-}
+// because the type is generic > we can use any type we want
+let pair1 = new KeyValuePair<string>('1', 'Apple')
+let pair2 = new KeyValuePair<number>(1, 'Apple')
+let pair3 = new KeyValuePair<boolean>(true, 'Apple')
 
-// we violate the DRY principle
-let pair = new StringKeyValuePair('1', 'Apple')
+// pair1.key. // now, it will show string methods
