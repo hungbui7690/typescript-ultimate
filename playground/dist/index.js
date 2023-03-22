3a1d62c0c86fb8c8c204e3f618c61416627bbee6
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 09:58:46 +0700

    Optional Chaining P4

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,13 +1,17 @@
 "use strict";
 /*
-  Optional Chaining P3
+  Optional Chaining P4
 
 */
-// with optional chaining, we can add more falsy type if we want > undefined
+var _a, _b, _c;
 function getCustomer(id) {
     return id === 0 ? null : { birthday: new Date() };
 }
 let customer1 = getCustomer(0);
-console.log(customer1 === null || customer1 === void 0 ? void 0 : customer1.birthday); // it still works
+console.log((_a = customer1 === null || customer1 === void 0 ? void 0 : customer1.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 let customer2 = getCustomer(1);
-console.log(customer2 === null || customer2 === void 0 ? void 0 : customer2.birthday);
+console.log((_b = customer2 === null || customer2 === void 0 ? void 0 : customer2.birthday) === null || _b === void 0 ? void 0 : _b.getFullYear()); // getFullYear()
+//////////////////////////////
+// this is useful when we work with array
+const numbers = [];
+console.log((_c = numbers[0]) === null || _c === void 0 ? void 0 : _c.toFixed());
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,19 +1,24 @@
 /*
-  Optional Chaining P3
+  Optional Chaining P4
 
 */
 
 type Customer = {
-  birthday: Date
+  birthday?: Date // we can also add here
 }
 
-// with optional chaining, we can add more falsy type if we want > undefined
 function getCustomer(id: number): Customer | null | undefined {
   return id === 0 ? null : { birthday: new Date() }
 }
 
 let customer1 = getCustomer(0)
-console.log(customer1?.birthday) // it still works
+console.log(customer1?.birthday?.getFullYear())
 
 let customer2 = getCustomer(1)
-console.log(customer2?.birthday)
+console.log(customer2?.birthday?.getFullYear()) // getFullYear()
+
+//////////////////////////////
+
+// this is useful when we work with array
+const numbers: number[] = []
+console.log(numbers[0]?.toFixed())
