cfc166d6cb215e646c64d050d62018126b097b2d
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 13:00:23 +0700

    Static Members P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
 class Ride {
+    constructor() {
+        this.activeRides = 0;
+    }
+    start() {
+        this.activeRides++;
+    }
+    stop() {
+        this.activeRides--;
+    }
 }
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(ride1.activeRides);
+console.log(ride2.activeRides);
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,25 @@
 /*
-  Static Members P1
+  Static Members P2
 
 */
 
-// for ex, we want to create an uber app
 class Ride {
-  // passenger
-  // pickup location
-  // dropOffLocation
+  activeRides: number = 0
+
+  start() {
+    this.activeRides++
+  }
+  stop() {
+    this.activeRides--
+  }
 }
+
+let ride1 = new Ride()
+ride1.start()
+
+let ride2 = new Ride()
+ride2.start()
+
+// both activeRides are independent to each other > we have 2 activeRides slot in the memory
+console.log(ride1.activeRides)
+console.log(ride2.activeRides)
