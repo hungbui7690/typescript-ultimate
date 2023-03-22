8b7b1f318211747f7c9febc3178917c7dafe1e1e
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 01:19:48 +0700

    What are Decorators?

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,15 @@
 /*
+  What are Decorators? 
+  - attributes that we can apply to class and members, so we can change the way they behave 
 
+  (***) > Angular, Vue has many decorators > @Python, @Component... 
+        > Decorators are just functions > are called at runtime and pass the class to it > it has the chance to modify the class : add properties, methods...
+
+  (***) because decorators are still in experimental > need to turn on in tsconfig
+        > "experimentalDecorators": true
 
 */
+
+// Example: TS does not have Component
+@Component
+class ProfileComponent {}
