5c23f0f1a39b43ecf4f14c42cce2cabcbb64fd9a
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:02:23 +0700

    Understand the Problem P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,11 @@
 /*
-
-
+  Understand the Problem P1
+  - what if in the app, somewhere we need to use string as key 
+    > with the current implementation, we cannot do this
 */
+
+class KeyValuePair {
+  constructor(public key: number, public value: string) {}
+}
+
+let pair = new KeyValuePair(1, 'Apple')
