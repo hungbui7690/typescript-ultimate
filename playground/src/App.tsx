13ed8a212f40abafabe439e1da04ac18b5854651
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 06:48:51 +0700

    Handling Events P2

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,7 +1,7 @@
 /*
-  Handling Events P1
-  - add delete button 
-    > ReminderList
+  Handling Events P2
+  - the data is in App.js > function need to create here
+  - but we need to pass the function through props > add function definition for props in ReminderList.tsx
 
 */
 
@@ -14,6 +14,11 @@ import reminderService from './services/reminder'
 function App() {
   const [reminders, setReminders] = useState<Reminder[]>([])
 
+  // ***
+  const removeReminder = (id: number) => {
+    console.log(id)
+  }
+
   const loadReminders = async () => {
     const reminders = await reminderService.getReminder()
     setReminders(reminders)
@@ -25,7 +30,7 @@ function App() {
 
   return (
     <div>
-      <ReminderList items={reminders} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   )
 }
--- playground/src/components/ReminderList.tsx
+++ playground/src/components/ReminderList.tsx
@@ -2,17 +2,20 @@ import Reminder from '../models/Reminder'
 
 export interface ReminderListProps {
   items: Reminder[]
+  onRemoveReminder: (id: number) => void // ***
 }
 
-const ReminderList = ({ items }: ReminderListProps) => {
+// ***
+const ReminderList = ({ items, onRemoveReminder }: ReminderListProps) => {
   return (
     <ul className='list-group'>
       {items.map((item) => (
         <li key={item.id} className='list-group-item'>
           {item.title}
-
-          {/* *** */}
-          <button className='btn btn-outline-danger mx-3 rounded-pill'>
+          <button
+            onClick={() => onRemoveReminder(item.id)} // ***
+            className='btn btn-outline-danger mx-3 rounded-pill'
+          >
             Delete
           </button>
         </li>
