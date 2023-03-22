ac2de5c5b67171618974bbb521166f1025be76a8
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 06:41:26 +0700

    Using the Effect Hook P2

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,7 @@
 /*
-  Using the Effect Hook P1
+  Using the Effect Hook P2
+  - apply some bootstrap class
+    > ReminderList
 
 */
 
@@ -7,21 +9,18 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import ReminderList from './components/ReminderList'
 import Reminder from './models/Reminder'
-
-import reminderService from './services/reminder' // import the service
+import reminderService from './services/reminder'
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([]) // remove sample data
+  const [reminders, setReminders] = useState<Reminder[]>([])
 
-  // ***
   const loadReminders = async () => {
     const reminders = await reminderService.getReminder()
     setReminders(reminders)
   }
 
-  // ***
   useEffect(() => {
-    loadReminders() // without await
+    loadReminders()
   }, [])
 
   return (
--- playground/src/components/ReminderList.tsx
+++ playground/src/components/ReminderList.tsx
@@ -5,10 +5,13 @@ export interface ReminderListProps {
 }
 
 const ReminderList = ({ items }: ReminderListProps) => {
+  // add some bootstrap classes
   return (
-    <ul>
+    <ul className='list-group'>
       {items.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li key={item.id} className='list-group-item'>
+          {item.title}
+        </li>
       ))}
     </ul>
   )
