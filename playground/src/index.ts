b45dc0d8c0cab194638f3db9fb24d4673cd18643
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 17:45:24 +0700

    Extending Generic Classes P3

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,5 +1,5 @@
 /*
-  Extending Generic Classes P2
+  Extending Generic Classes P3
 
 */
 
@@ -9,12 +9,17 @@ interface Product {
 }
 
 class Store<T> {
-  private _items: T[] // private
+  private _items: T[]
 
   add(item: T): void {
     this._items.push(item)
   }
 }
 
-let store = new Store<Product>()
-store._items = [] // error if we want to change _items
+///////////////////////////////////////
+
+// Scenario 1: err
+class CompressibleStore extends Store<T> {}
+
+// if we create an instance like this, we don't have a chance to put the type
+new CompressibleStore<Product>()
