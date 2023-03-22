f376bb5bf129511d50f368ba4d757815c7bbc84d
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 06:21:23 +0700

    Using the State Hook P1

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,22 +1,23 @@
 /*
-  Creating a Component P3
+  Using the State Hook P1
 
 */
 
+import { useState } from 'react'
 import './App.css'
 import ReminderList from './components/ReminderList'
-import Reminder from './models/Reminder' // (***)
+import Reminder from './models/Reminder'
 
-// ***
 const reminders: Reminder[] = [
   { id: 1, title: 'Reminder 1' },
   { id: 2, title: 'Reminder 2' },
 ]
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>() // Reminder[] | undefined > undefined because we did not initialize > if we don't supply arg, undefined will be passed
+
   return (
     <div>
-      {/* *** */}
       <ReminderList items={reminders} />
     </div>
   )
