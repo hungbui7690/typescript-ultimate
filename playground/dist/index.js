1b79b20edc808b9ee52c9d763dd4017c2a1027f4
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 12:56:39 +0700

    Index Signature P3

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 class SeatAssignment {
 }
+let seats = new SeatAssignment();
+seats.A1 = 'John';
+seats['A2'] = 'Nick';
+console.log(seats);
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,12 +1,15 @@
 /*
-  Index Signature P2
+  Index Signature P3
 
 */
 
-// we want to assign seats (A1, A2) to multiple people
-// A1: 'John', A2: 'Mosh', A3...
-// but we don't want to do this way
+// this is index signature
 class SeatAssignment {
-  A1: string
-  A2: string
+  [seatNumber: string]: string
 }
+
+let seats = new SeatAssignment()
+seats.A1 = 'John'
+seats['A2'] = 'Nick'
+
+console.log(seats)
