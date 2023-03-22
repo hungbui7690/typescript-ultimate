e7274bcdff723ffe0b35223f9a741a59e33b0680
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 09:02:11 +0700

    Functions P7

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,11 +1,11 @@
-'use strict'
+"use strict";
 function calculateTax(income, taxYear) {
-  if (taxYear < 2022) {
-    return income * 1.2
-  }
-  return income * 1.3
+    if ((taxYear || 2022) < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
 }
-const tax = calculateTax(40000, 2023)
-const x = calculateTax()
-console.log(tax)
+const tax1 = calculateTax(40000, 2023);
+const tax2 = calculateTax(40000);
+console.log(tax1, tax2);
 //# sourceMappingURL=index.js.map
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,17 +1,17 @@
 /*
-  Functions P6
+  Functions P7
 
 */
 
-// optional > ?: > hover taxYear > if we don't supply taxYear > taxYear = undefined
 function calculateTax(income: number, taxYear?: number): number {
-  if (taxYear < 2022) {
+  // first solution: good old ES5
+  if ((taxYear || 2022) < 2022) {
     return income * 1.2
   }
 
   return income * 1.3
 }
 
-// there's no way to catch these error in JS
-const tax1 = calculateTax(40_000, 2023, 1)
+const tax1 = calculateTax(40_000, 2023)
 const tax2 = calculateTax(40_000)
+console.log(tax1, tax2)
