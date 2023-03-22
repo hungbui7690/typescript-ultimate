98c56e968cc101b9dbeee52120bd9c5f0591b767
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 10:00:30 +0700

    Optional Chaining P5

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,17 +1,8 @@
 "use strict";
 /*
-  Optional Chaining P4
+  Optional Chaining P5
 
 */
-var _a, _b, _c;
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer1 = getCustomer(0);
-console.log((_a = customer1 === null || customer1 === void 0 ? void 0 : customer1.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
-let customer2 = getCustomer(1);
-console.log((_b = customer2 === null || customer2 === void 0 ? void 0 : customer2.birthday) === null || _b === void 0 ? void 0 : _b.getFullYear()); // getFullYear()
-//////////////////////////////
-// this is useful when we work with array
-const numbers = [];
-console.log((_c = numbers[0]) === null || _c === void 0 ? void 0 : _c.toFixed());
+let log = (message) => console.log(message);
+log = null;
+log === null || log === void 0 ? void 0 : log('a'); // optional chaining when calling a function
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,24 +1,9 @@
 /*
-  Optional Chaining P4
+  Optional Chaining P5
 
 */
 
-type Customer = {
-  birthday?: Date // we can also add here
-}
+let log: any = (message: string) => console.log(message)
+log = null
 
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { birthday: new Date() }
-}
-
-let customer1 = getCustomer(0)
-console.log(customer1?.birthday?.getFullYear())
-
-let customer2 = getCustomer(1)
-console.log(customer2?.birthday?.getFullYear()) // getFullYear()
-
-//////////////////////////////
-
-// this is useful when we work with array
-const numbers: number[] = []
-console.log(numbers[0]?.toFixed())
+log?.('a') // optional chaining when calling a function
