09266f5b8a5213341df5bc6ec2af8168380aac41
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 16:03:59 +0700

    Understand the Problem P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,12 @@
 /*
-  Understand the Problem P1
-  - what if in the app, somewhere we need to use string as key 
-    > with the current implementation, we cannot do this
+  Understand the Problem P2
+
 */
 
 class KeyValuePair {
-  constructor(public key: number, public value: string) {}
+  // Solution 1: use any type (not recommended)
+  constructor(public key: any, public value: string) {}
 }
 
 let pair = new KeyValuePair(1, 'Apple')
+// pair.key. // no methods are shown since type is any > if we change type to string > show string methods
