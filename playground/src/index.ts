a3d36d3fddfaf0212de581acb830a9e7ae4b3d93
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 04:33:58 +0700

    Type Checking JS Code P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,10 @@
 /*
-  Include JS Code in TS Projects P2
-  - "allowJs": true
-  - "module": "commonjs" > must be commonjs 
+  Type Checking JS Code P1
 
 */
 
 import { calculateTax } from './tax'
 
-let tax = calculateTax(1000)
-console.log(tax)
+// with JS, if we don't provide parameter > no err
+let tax = calculateTax()
+console.log(tax) // NaN
