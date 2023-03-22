084da01db42634b50772a1b5e99e4fb84c6842c8
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 03:57:47 +0700

    Exporting & Importing P3

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,13 +1,13 @@
 /*
-  Exporting & Importing P2
-  - create shapes.ts
+  Exporting & Importing P3
 
 */
 
-// import and set alias
 import { Circle as MyCircle } from './shapes'
 
 let circle = new MyCircle(1)
-console.log(circle.radius)
 
-let square = new Square(1) // since we did not import > place cursor on "Square", then Ctrl + .
+// ctrl + .  >> move to new file
+interface Product {
+  name: string
+}
