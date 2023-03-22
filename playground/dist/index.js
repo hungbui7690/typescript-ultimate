e9ae36e98f3b6a7a0b70757cb6119b83b1463719
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 02:20:08 +0700

    Accessor Decorators P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,16 +1,31 @@
 "use strict";
 /*
-  Accessor Decorators P1
-  - getters & setters
-
+  Accessor Decorators P2
+  - accessor decorators are similar to method decorators
+  
 */
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// (***)
+function Capitalize(target, methodName, descriptor) {
+    const original = descriptor.get; // instead use .value > we use .get
+    descriptor.get = function () {
+        original === null || original === void 0 ? void 0 : original.call(this); // use optional chaining, because type of original is any|undefined
+    };
+}
 class Person {
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    // getter
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
     }
 }
+__decorate([
+    Capitalize // apply decorator here
+], Person.prototype, "fullName", null);
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,13 +1,25 @@
 /*
-  Accessor Decorators P1
-  - getters & setters
-
+  Accessor Decorators P2
+  - accessor decorators are similar to method decorators
+  
 */
+
+// (***)
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get // instead use .value > we use .get
+  descriptor.get = function () {
+    original?.call(this) // use optional chaining, because type of original is any|undefined
+  }
+}
 
 class Person {
   constructor(public firstName: string, public lastName: string) {}
 
-  // getter
+  @Capitalize // apply decorator here
   get fullName() {
     return `${this.firstName} ${this.lastName}`
   }
