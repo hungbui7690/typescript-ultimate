dbe3bd867e9d2b3aa1d94531f90aaee857f93a51
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 02:12:40 +0700

    Method Decorators P7

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
-  Method Decorators P6
+  Method Decorators P7
 
 */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -9,12 +9,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// we still have some err at the param list > because "noUnusedParameters": true > need to turn off
 function Log(target, methodName, descriptor) {
     const original = descriptor.value;
-    // if we don't want to hard code the message > add parameter here
+    // (***) problem: now we can only apply decorator to a function that has this signature fn(message) > not flexible > fix in next lecture
     descriptor.value = function (message) {
         console.log('Before');
-        original.call(this, message); // use here
+        original.call(this, message);
         console.log('After');
     };
 }
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,15 +1,16 @@
 /*
-  Method Decorators P6
+  Method Decorators P7
 
 */
 
+// we still have some err at the param list > because "noUnusedParameters": true > need to turn off
 function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
   const original = descriptor.value as Function
 
-  // if we don't want to hard code the message > add parameter here
+  // (***) problem: now we can only apply decorator to a function that has this signature fn(message) > not flexible > fix in next lecture
   descriptor.value = function (message: string) {
     console.log('Before')
-    original.call(this, message) // use here
+    original.call(this, message)
     console.log('After')
   }
 }
