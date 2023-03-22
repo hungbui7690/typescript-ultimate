ac07813a5778cce9e2908a3ec0af2b6e04c8c758
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 12:42:39 +0700

    Access Control Keyword P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -3,14 +3,14 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid Amount!!');
-        this.balance += amount;
+        this._balance += amount;
     }
 }
 let account = new Account(1, 'bic', 123);
-account.balance = 100000;
+account._balance = 100000;
 console.log(account);
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,28 +1,28 @@
 /*
-  Access Control Keyword P1
+  Access Control Keyword P2
+  - default = public
 
 */
 
 class Account {
   readonly id: number
   owner: string
-  balance: number
+  private _balance: number // we use private keyword here > convention is to use _ for private property
   nickName?: string
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id
     this.owner = owner
-    this.balance = balance
+    this._balance = balance
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error('Invalid Amount!!')
-    // (***) what if we have array of transaction here
-    this.balance += amount
+    this._balance += amount
   }
 }
 
 let account = new Account(1, 'bic', 123)
 
-account.balance = 100_000 // (***) the problem appears when we update balance here > we did not update the transaction array > we don't know who pay
+account._balance = 100_000 // complain here
 console.log(account)
