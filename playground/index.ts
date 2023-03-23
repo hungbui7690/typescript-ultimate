07da3686fa11955be454ab442214f02f42e57864
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 10:07:08 +0700

    Parsing Request Bodies P3

--- playground/index.ts
+++ playground/index.ts
@@ -1,6 +1,10 @@
 /*
-  Parsing Request Bodies P2
-  - routes/reminders.ts
+  Parsing Request Bodies P3
+  - by default, express does not parse req.body > need to install middleware
+    > app.use(express.json())
+
+  - move interface to another file
+    > create dtos/CreateReminderDtos.ts
 
 */
 
@@ -9,10 +13,10 @@ const app = express()
 
 import remindersRouter from './routes/reminders'
 
+app.use(express.json()) // ***
 app.use('/reminders', remindersRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-
 app.listen(5000, () => console.log('Server Started...'))
--- playground/routes/reminders.ts
+++ playground/routes/reminders.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express'
 
+import CreateReminderDto from '../dtos/CreateReminderDto' // ***
+
 const router = Router()
 
 router.get('/', (req, res) => {
   res.send('Hello from Router!!')
 })
 
-// *** Dto = Data Transfer Object
-interface CreateReminderDto {
-  title: string
-}
-
 router.post('/', (req, res) => {
   const { title } = req.body as CreateReminderDto // ***
-  res.json(title) // test with postman > TypeError: Cannot destructure property 'title' of 'req.body' as it is undefined. > next lesson
+  res.json(title)
 })
 
 export default router
