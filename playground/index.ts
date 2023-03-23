8c9767d3e7010fa7cc99438decf3c61779d27832
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 09:59:17 +0700

    Creating a Router

--- playground/index.ts
+++ playground/index.ts
@@ -1,13 +1,19 @@
 /*
-  Creating a Basic Route
+  Creating a Router
+  - create routes/reminder.ts
   
 */
 
 import express from 'express'
 const app = express()
 
+import remindersRouter from './routes/reminders' // ***
+
+// *** test with /reminders
+app.use('/reminders', remindersRouter)
+
 app.get('/', (req, res) => {
-  res.send('Hello World') // since we install @types > we have all the methods attached with req & res
+  res.send('Hello World')
 })
 
 app.listen(5000, () => console.log('Server Started...'))
