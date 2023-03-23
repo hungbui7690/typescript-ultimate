2f1abc26c80c925e39ea8cf05c222d44ff0d427a
hungbui7690 <hungbui7690@gmail.com>
2023-03-23 09:55:38 +0700

    Creating a Basic Route

--- playground/index.ts
+++ playground/index.ts
@@ -1,18 +1,13 @@
 /*
-  Setting Up an Express Project
-  > npm init -y
-  > npm i express
-  > npm i -D typescript @types/node @types/express
-  > tsc --init
-  > npm install -D nodemon
-  > package.json: "nodemon index.ts"
+  Creating a Basic Route
   
-  (***) we have to install typescript though we have TS in our machine just because we want to freeze the version of TS that we use > also, we if other devs don't have TS on their machine, they can install through package.json
-  (***) latest version of nodemon supports TS
-
 */
 
 import express from 'express'
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World') // since we install @types > we have all the methods attached with req & res
+})
 
 app.listen(5000, () => console.log('Server Started...'))
